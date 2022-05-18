b9eee39b28eba52fecd7ea6e5f4beca142168a5f
Timemash <timemash24@gmail.com>
2022-05-18 19:35:58 +0900

    Simplify codes in index.html and index.js Unuse style.css

--- formSubmitEvent/src/index.js
+++ formSubmitEvent/src/index.js
@@ -1,26 +1,22 @@
-const maxNumForm = document.querySelector("#maxNum-form");
-const maxNumInput = document.querySelector("#maxNum-form input");
-const guessNumForm = document.querySelector("#guessNum-form");
-const guessNumInput = document.querySelector("#guessNum-form input");
-const resultNum = document.querySelector("#resultNum");
-const resultWin = document.querySelector("#resultWin");
+const guessForm = document.getElementById("js-guess");
+const result = document.getElementById("js-result");
+const maxNumber = document.getElementById("maxNumber");
 
-function onGuessNumSubmit(event) {
-  event.preventDefault();
-  const maxNum = maxNumInput.value;
-  const guessNum = guessNumInput.value;
-  const randomNum = generateNumber(maxNum);
-  resultNum.innerText = `You chose: ${guessNum}, the machine chose ${randomNum}.`;
-  if (+randomNum === +guessNum) {
-    resultWin.innerText = "You won!";
-  } else {
-    resultWin.innerText = "You lost!";
+function handleGuessSubmit(e) {
+  e.preventDefault();
+  const guessInput = guessForm.querySelector("input");
+  if (guessInput.value === "" && maxNumber === "") {
+    return;
   }
+  const max = maxNumber.value;
+  const random = Math.ceil(Math.random() * max);
+  const userGuess = parseInt(guessInput.value, 10);
+  const resultSpan = result.querySelector("span");
+  resultSpan.innerHTML = `
+  You chose: ${userGuess},
+  the machine chose: ${random}.<br />
+  <strong>${userGuess === random ? "You won!" : "You lost!"}</strong>
+  `;
 }
 
-function generateNumber(max) {
-  return Math.floor(Math.random() * max + 1);
-}
-
-maxNumForm.addEventListener("submit", onGuessNumSubmit);
-guessNumForm.addEventListener("submit", onGuessNumSubmit);
+guessForm.addEventListener("submit", handleGuessSubmit);
